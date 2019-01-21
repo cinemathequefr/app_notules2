b1edea3ec906106e20b77185cd1ac8fa50bdb2fc
nltesown <nlte@magic.fr>
2019-01-21 12:46:19 +0100

    merge genere des donnes a plat (= items de seances) + template du mode de tri 1

--- merge.js
+++ merge.js
@@ -16,29 +16,46 @@ const writeFile = promisify(fs.writeFile);
 const idCycle = parseInt(process.argv[2], 10); // Id de cycle saisie en paramètre de ligne de commande
 
 (async function () {
-
-  const cyclesConfig = await helpers.readFileAsJson("./data/config/cycles.json");
+  const cyclesConfig = await helpers.readFileAsJson(
+    "./data/config/cycles.json"
+  );
   let cycleConfig = _(cyclesConfig).find({
     idCycleProg: idCycle
   });
 
   const filename = getFilenameFromCycle(idCycle, cyclesConfig);
 
-  let seances = await helpers.readFileAsJson(`./data/cycles/${filename.seances}.json`);
-  let films = await helpers.readFileAsJson(`./data/cycles/${filename.films}.json`);
+  let seances = await helpers.readFileAsJson(
+    `./data/cycles/${filename.seances}.json`
+  );
+  let films = await helpers.readFileAsJson(
+    `./data/cycles/${filename.films}.json`
+  );
 
-
-  let filmsInSeances = _(seances).map(d => _(d.items).map(e => e.idFilm).value()).flatten().uniq().sort().value();
-  let filmsInFilms = _(films).map(d => d.idFilm).sort().value();
+  let filmsInSeances = _(seances)
+    .map(d =>
+      _(d.items)
+      .map(e => e.idFilm)
+      .value()
+    )
+    .flatten()
+    .uniq()
+    .sort()
+    .value();
+  let filmsInFilms = _(films)
+    .map(d => d.idFilm)
+    .sort()
+    .value();
   let cycle = []; // Données du cycle, fusion de séances dans les films.
-
 
   // Vérification de la cohérence des deux fichiers avant fusion
   // Note: Cette vérification est nécessaire car le fichier films.json sera généralement généré (pour corrections)
   // bien avant le fichier seances.json.
   // Il faudra pouvoir mettre à jour film.json en ajoutant (ou retirant) des films pour retrouver la cohérence avec les séances.
   console.log(`Les données films contiennent ${filmsInFilms.length} films.`);
-  console.log(`Les données séances référencent ${filmsInSeances.length} films.`);
+  console.log(
+    `Les données séances référencent ${filmsInSeances.length} films.`
+  );
 
   let diffFilmsSeances = _.difference(filmsInFilms, filmsInSeances);
   let diffSeancesFilms = _.difference(filmsInSeances, filmsInFilms);
@@ -49,11 +66,19 @@ const idCycle = parseInt(process.argv[2], 10); // Id de cycle saisie en paramèt
     console.log("Les données ne sont pas cohérentes");
     if (diffFilmsSeances.length > 0) {
       // Cas bénin, il suffit d'ignorer les films concernés
-      console.log(`Aucune séance n'a été trouvée pour les films suivants : ${diffFilmsSeances.join(", ")}`);
+      console.log(
+        `Aucune séance n'a été trouvée pour les films suivants : ${diffFilmsSeances.join(
+          ", "
+        )}`
+      );
     }
     if (diffSeancesFilms.length > 0) {
       // Il faudra pouvoir patcher films.json avec les données manquantes
-      console.log(`Les films suivants ont des séances mais pas de données les concernant : ${diffFilmsSeances.join(", ")}`);
+      console.log(
+        `Les films suivants ont des séances mais pas de données les concernant : ${diffFilmsSeances.join(
+          ", "
+        )}`
+      );
     }
   }
 
@@ -61,43 +86,103 @@ const idCycle = parseInt(process.argv[2], 10); // Id de cycle saisie en paramèt
 
   seances = _(seances)
     .map(d => {
-      let header = _(d).omit("items").value();
-      return _(d.items).map(e => _({}).assign(e, header).value()).value();
+      let header = _(d)
+        .omit("items")
+        .value();
+      return _(d.items)
+        .map(e =>
+          _({})
+          .assign(e, header)
+          .value()
+        )
+        .value();
     })
     .flatten()
     .value();
 
-  films = _(films).groupBy("idFilm").mapValues(e => e[0]).value();
+  films = _(films)
+    .groupBy("idFilm")
+    .mapValues(e => e[0])
+    .value();
 
-  seances = _(seances).groupBy("idFilm").mapValues(e => {
-    return {
-      seance: e
-    }
-  }).value();
+  seances = _(seances)
+    .groupBy("idFilm")
+    .mapValues(e => {
+      return {
+        seance: e
+      };
+    })
+    .value();
 
   cycle = _.merge(films, seances);
-  cycle = _(cycle).map().value();
+  cycle = _(cycle)
+    .map()
+    .value();
 
   // Répartition par sous-cycle (en suivant l'ordre indiqué par cycleConfig)
-  cycle = _(cycleConfig.sousCycles).map(d => {
-    return {
-      titreSousCycle: d.titre,
-      tri: d.tri,
-      items: _(d.cats).map(e =>
-        _(cycle).groupBy("idCategorie").value()[e]
-      ).flatten().orderBy("titre").value()
-    };
-  }).value();
+  cycle = _(cycleConfig.sousCycles)
+    .map(d => {
+      return {
+        titreSousCycle: d.titre,
+        tri: d.tri,
+        items: _(d.cats)
+          .map(
+            e =>
+            _(cycle)
+            .groupBy("idCategorie")
+            .value()[e]
+          )
+          .flatten()
+          .orderBy("titre")
+          .value()
+      };
+    })
+    .value();
+
+  cycle = _(cycle)
+    .map(d =>
+      _({}).assign(d, {
+        items: _(d.items)
+          .map(e =>
+            _(e.seance)
+            .map(f =>
+              _({})
+              .assign(
+                f,
+                _(e)
+                .pick([
+                  "titre",
+                  "art",
+                  "titreVo",
+                  "artVo",
+                  "realisateurs",
+                  "annee",
+                  "pays",
+                  "generique",
+                  "adaptation",
+                  "textes"
+                ])
+                .value()
+              )
+              .value()
+            )
+            .value()
+          )
+          .sortBy("titre", "dateHeure")
+          .flatten()
+          .value()
+      })
+    )
+    .value();
 
   await writeFile(
-    `data/cycles/CYCLE${cycleConfig.idCycleProg}_ALL ${cycleConfig.titreCycle}.json`,
+    `data/cycles/CYCLE${cycleConfig.idCycleProg}_ALL ${
+      cycleConfig.titreCycle
+    }.json`,
     JSON.stringify(cycle, null, 2),
     "utf8"
   );
-
 })();
-
-
 
 // Obtient les noms de fichiers json d'un cycle (qui incluent le titre du cycle en clair) à partir de son id et de l'objet de configuration des cycles.
 function getFilenameFromCycle(idCycle, cyclesConfig) {
@@ -107,5 +192,5 @@ function getFilenameFromCycle(idCycle, cyclesConfig) {
   return {
     films: `CYCLE${cycleConfig.idCycleProg}_FILMS ${cycleConfig.titreCycle}`,
     seances: `CYCLE${cycleConfig.idCycleProg}_SEANCES ${cycleConfig.titreCycle}`
-  }
+  };
 }
--- web/main.js
+++ web/main.js
@@ -1,4 +1,5 @@
-var path = "CYCLE408_ALL Federico Fellini.json";
+var path = "CYCLE370_ALL La Nouvelle Vague tchèque... et après.json";
+// var path = "CYCLE408_ALL Federico Fellini.json";
 // var path = "CYCLE400_ALL Jerzy Skolimowski.json";
 // var path = "CYCLE357_ALL Éric Rohmer.json";
 
@@ -45,85 +46,185 @@ moment.locale("fr", {
   weekdaysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
 });
 
-
+// Fonctions de tri dans les sous-cycles
+var tri = {
+  "1": function (items) {
+    // Tri par film
+    return _(items)
+      .groupBy("idFilm")
+      .map(d =>
+        _(d[0])
+        .pick([
+          "idCycle",
+          "idFilm",
+          "titre",
+          "art",
+          "titreVo",
+          "artVo",
+          "realisateurs",
+          "annee",
+          "pays",
+          "textes",
+          "adaptation",
+          "generique",
+          "duree",
+          "format",
+          "version"
+        ])
+        .assign({
+          seance: _(d)
+            .map(e =>
+              _(e)
+              .pick([
+                "idSeance",
+                "ordre",
+                "idEvenement",
+                "dateHeure",
+                "idSalle",
+                "mention"
+              ])
+              .assign({
+                avant: _(items)
+                  .filter(
+                    f => f.idSeance === e.idSeance && f.ordre < e.ordre
+                  )
+                  .thru(g =>
+                    g.length > 0 ?
+                    _(g)
+                    .map(g =>
+                      _(g)
+                      .pick(["idFilm", "titre", "art"])
+                      .value()
+                    )
+                    .value() :
+                    null
+                  )
+                  .value(),
+                apres: _(items)
+                  .filter(
+                    f => f.idSeance === e.idSeance && f.ordre > e.ordre
+                  )
+                  .thru(g =>
+                    g.length > 0 ?
+                    _(g)
+                    .map(g =>
+                      _(g)
+                      .pick(["idFilm", "titre", "art"])
+                      .value()
+                    )
+                    .value() :
+                    null
+                  )
+                  .value()
+              })
+              .value()
+            )
+            .value()
+        })
+        .value()
+      )
+      .orderBy("titre")
+      .value();
+  },
+  "2": function (d) {
+    return _(items)
+      .sortBy("titre")
+      .value();
+  },
+  "3": function (d) {
+    return _(items)
+      .sortBy("titre")
+      .value();
+  }
+};
 
 var temp = {
-  cycle1: _.template(`
+  cycle: _.template(`
 <% _.forEach(data, function (d) { %>
   <div class="souscycle">
     <h2><%= d.titreSousCycle %></h2>
-    <% _.forEach(d.items, function (f) { %>
-      <div class="film">
-        <div class="titrefilm"><%= artTitre(f.art, f.titre) %></div>
-        <% if(f.titreVo) { %><div class="titreFilm">(<%= artTitre(f.artVo, f.titreVo) %>)</div><% } %>
-        <div><%= de(f.realisateurs) + f.realisateurs %></div>
-        <div><%= joinLast(" / ", " / ", [f.pays, f.annee, (f.seance[0].duree ? f.seance[0].duree + "'" : null) , f.seance[0].version, f.seance[0].format]) %></div>
-        <% if (f.adaptation) { %><div><%= f.adaptation %></div><% } %>
-        <% if (f.generique) { %>Avec <%= f.generique %>.<% } %>
-        <% _.forEach(f.textes, function (t) { %>
-          <p><%= t.texte %></p>
-        <% }); %>
-        <% _.forEach(f.seance, function (s) { %>
-          <div class="seance">
-            <div><%= moment(s.dateHeure).format("ddd D MMM HH[h]mm") %> <span class="salle"><%= s.idSalle[0] %></span></div>
-            <% if (s.mention) { %><div class="mentionSeance"><%= s.mention %></div><% } %>
-          </div>
-        <% }); %>
-      </div>
-    <% }); %>
+    <% if (d.tri === 1) { %>
+      <% _.forEach(tri[1](d.items), function (e) { %>
+        <div class="item">
+          <div class="titrefilm"><%= artTitre(e.art, e.titre) %></div>
+          <% if(e.titreVo) { %><div class="titreFilm">(<%= artTitre(e.artVo, e.titreVo) %>)</div><% } %>
+          <div><%= joinLast(" / ", " / ", [e.pays, e.annee, (e.duree ? e.duree + "'" : null) , e.version, e.format]) %></div>
+          <% if (e.adaptation) { %><div><%= e.adaptation %></div><% } %>
+          <% if (e.generique) { %>Avec <%= e.generique %>.<% } %>
+          <% _.forEach(e.textes, function (f) { %>
+            <p><%= f.texte %></p>
+          <% }); %>          
+          <% _.forEach(e.seance, function (f) { %>
+            <div class="seance">
+            <div><%= moment(f.dateHeure).format("ddd D MMM HH[h]mm") %> <span class="salle"><%= f.idSalle[0] %></span></div>
+            <div><%= precedeSuivi(f.avant, f.apres) %></div>
+            <% if (f.mention) { %><div class="mentionSeance"><%= f.mention %></div><% } %>
+            </div>
+          <% }); %>
+        </div>
+      <% }); %>
+    <% } %>
   </div>
 <% }); %>
-`)
+  `)
 };
-
 
 Promise.all([cycleData, documentReady()]).then(run);
 
 function run() {
   var data = arguments[0][0];
   var $cont = $(".container");
-
-
-
-  // Tri dans les sous-cycles
-  var tri = {
-    "1": function (d) {
-      return _(d.items).sortBy("titre").value();
-    },
-    "2": function (d) {
-      return _(d.items).sortBy("titre").value();
-    },
-    "3": function (d) {
-      return _(d.items).sortBy("titre").value();
-    },
-  }
-
-  data = _(data).map(d => _(d).assign({
-    items: tri[d.tri](d)
-  }).value()).value();
-
-  console.log(data);
-
-
-
-  var o = temp.cycle1({
+  var o = temp.cycle({
     data: data
   });
-
-
-
-
-
   $cont.html(o);
-  // $cont.html(JSON.stringify(data, null, 2));
-
-
-
 }
 
+// Fonctions de formatage
+function joinLast(separator, lastSeparator, arr) {
+  separator = separator || "";
+  lastSeparator = lastSeparator || separator;
+  arr = _(arr)
+    .filter(i => !!i)
+    .value(); // Elimine les items falsy
+  function j(a) {
+    a = a || [];
+    if (a.length < 2) return a.join("");
+    var last = a.pop();
+    return a.join(separator) + lastSeparator + last;
+  }
+  return j(arr);
+}
+
+function precedeSuivi(avant, apres) {
+  avant = joinLast(
+    ", ",
+    ", ",
+    _(avant).map(function (d) {
+      return "_" + artTitre(d.art, d.titre) + "_";
+    })
+  );
+  apres = joinLast(
+    ", ",
+    ", ",
+    _(apres).map(function (d) {
+      return "_" + artTitre(d.art, d.titre) + "_";
+    })
+  );
+  if (avant !== "" && apres != "") {
+    return `Film précédé ${de(avant) + avant} et suivi de ${de(apres) +
+      apres}.`;
+  } else if (avant !== "") {
+    return `Film précédé ${de(avant) + avant}.`;
+  } else if (apres !== "") {
+    return `Film suivi ${de(apres) + apres}.`;
+  } else {
+    return "";
+  }
+}
 
 function artTitre(art, titre) {
-  return (!art ? titre : (art === "L'" ? art + titre : art + " " + titre));
+  return !art ? titre : art === "L'" ? art + titre : art + " " + titre;
 }
 
 /**
@@ -135,38 +236,10 @@ function artTitre(art, titre) {
  */
 function de(str) {
   if (!str) return;
-  return (_.indexOf("AEIOU", _.upperCase(_.deburr(str)).charAt(0)) > -1 ? "d'" : "de ");
+  return _.indexOf("AEIOU", _.upperCase(_.deburr(str)).charAt(0)) > -1 ?
+    "d'" :
+    "de ";
 }
-
-/**
- * joinLast
- * @description
- * Fonction équivalent à la méthode `Array.join` mais permettant de spécifier un séparateur particulier pour la dernière position.
- * A la différence de `join`, gère correctement le cas où `arr` est `undefined`.
- * Cas évident : la conjonction "et" en fin de liste.
- * @param {string} separator Séparateur d'items
- * @param {string} lastSeparator Séparateur d'items pour la dernière position (entre l'avant-dernier et le dernier item)
- * @param {array} arr Tableau des items de liste à joindre
- * @return {function|string}
- */
-function joinLast(separator, lastSeparator, arr) {
-  separator = separator || "";
-  lastSeparator = lastSeparator || separator;
-
-  arr = _(arr).filter(i => !!i).value(); // Elimine les items falsy
-
-  function j(a) {
-    a = a || [];
-    if (a.length < 2) return a.join("");
-    var last = a.pop();
-    return a.join(separator) + lastSeparator + last;
-  }
-  return j(arr);
-}
-
-
-
-
 
 function documentReady() {
   if (document.readyState === "complete") {
